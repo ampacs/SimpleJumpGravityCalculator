3beda26b64a6c889b978fbad24714b42f5ad01f9
André Santiago <andrempacsantiago@outlook.com>
2021-01-28 18:48:01 +0000

    Added `GravityForceCalculator` from jump height, hanging time & apex fraction

---
+++ Assets/Physics/Gravity/GravityForce.cs
@@ -0,0 +1,14 @@
+﻿namespace Physics.Gravity
+{
+    public readonly struct GravityForce
+    {
+        public readonly float Upwards;
+        public readonly float Downwards;
+
+        public GravityForce(float upwards, float downwards)
+        {
+            Upwards   = upwards;
+            Downwards = downwards;
+        }
+    }
+}
---
+++ Assets/Physics/Gravity/GravityForceCalculator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Physics.Gravity
+{
+    public static class GravityForceCalculator
+    {
+        private const float MinimumDeviation = 1e-5f;
+
+        public static GravityForce GetGravityForce (float jumpHeight, float jumpTime, float jumpApexFraction = .5f)
+        {
+            jumpHeight = Mathf.Max(jumpHeight, 0);
+            jumpApexFraction = Mathf.Clamp(jumpApexFraction, MinimumDeviation, 1f - MinimumDeviation);
+
+            float upwards   = GetForce(jumpHeight, jumpTime, jumpApexFraction);
+            float downwards = GetForce(jumpHeight, jumpTime, 1 - jumpApexFraction);
+
+            return new GravityForce(upwards, downwards);
+        }
+
+        private static float GetForce(float jumpHeight, float jumpTime, float jumpApexFraction)
+        {
+            return 2 * jumpHeight / (jumpTime * jumpTime * jumpApexFraction * jumpApexFraction);
+        }
+    }
+}
