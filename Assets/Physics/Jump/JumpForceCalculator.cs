fb0cacdf08be7bf8ba6b05ee25a9dd2c265aa801
André Santiago <andrempacsantiago@outlook.com>
2021-01-28 18:48:39 +0000

    Added `JumpForceCalculator` from gravity intensity, jump height & object mass

---
+++ Assets/Physics/Jump/JumpForceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Physics.Jump
+{
+    public static class JumpForceCalculator
+    {
+        public static float GetJumpForce(float gravity, float jumpHeight, float mass = 1f)
+        {
+            gravity    = Mathf.Max(gravity,    0);
+            jumpHeight = Mathf.Max(jumpHeight, 0);
+            mass       = Mathf.Max(mass,       0);
+
+            return Mathf.Sqrt(2 * gravity * jumpHeight) * mass;
+        }
+    }
+}
